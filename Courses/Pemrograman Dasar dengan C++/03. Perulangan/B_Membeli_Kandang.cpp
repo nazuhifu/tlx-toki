5cc1c5aada084a406b1b3e1a5c69e042256344d7
Mohammad Nazhiif <nazhiifalghoniy@gmail.com>
2025-08-02 21:20:37 +0700

    commit

---
+++ Courses/Pemrograman Dasar dengan C++/03. Perulangan/B_Membeli_Kandang.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12;
+
+    // bulan pertama
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+
+    // bulan kedua
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+
+    // bulan ketiga
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+}
+// Output:
+// 19
+// 26
+// 33
---
+++ Courses/Pemrograman Dasar dengan C++/03. Perulangan/C_Perkenalan_While.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12, counter = 0;
+
+    while (counter < 3) {
+        luas_kandang += 7;
+        cout << luas_kandang << endl;
+
+        counter++;
+    }
+}
